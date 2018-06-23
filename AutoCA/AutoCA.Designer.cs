4ef78f566c9efecf2953e0aaac188ba5c4a10a95
Long Ha <long2585@LIVE.COM>
2018-06-23 09:13:31 +0700

    Create project

---
+++ AutoCA/AutoCA.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace AutoCA
+{
+    partial class AutoCA
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AutoCA));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txbUserName = new System.Windows.Forms.TextBox();
+            this.txbPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txbGroup = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.ckbAutoClose = new System.Windows.Forms.CheckBox();
+            this.txbAffected = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txbAssignee = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(2, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "UserName";
+            // 
+            // txbUserName
+            // 
+            this.txbUserName.Location = new System.Drawing.Point(94, 28);
+            this.txbUserName.Name = "txbUserName";
+            this.txbUserName.Size = new System.Drawing.Size(106, 20);
+            this.txbUserName.TabIndex = 1;
+            // 
+            // txbPassword
+            // 
+            this.txbPassword.Location = new System.Drawing.Point(94, 54);
+            this.txbPassword.Name = "txbPassword";
+            this.txbPassword.PasswordChar = '*';
+            this.txbPassword.Size = new System.Drawing.Size(106, 20);
+            this.txbPassword.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(2, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // txbName
+            // 
+            this.txbName.Location = new System.Drawing.Point(410, 36);
+            this.txbName.Name = "txbName";
+            this.txbName.Size = new System.Drawing.Size(106, 20);
+            this.txbName.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(318, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Name (suggest)";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txbUserName);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txbPassword);
+            this.groupBox1.Location = new System.Drawing.Point(15, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(220, 100);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "SignIn Information";
+            // 
+            // txbGroup
+            // 
+            this.txbGroup.Location = new System.Drawing.Point(410, 66);
+            this.txbGroup.Name = "txbGroup";
+            this.txbGroup.Size = new System.Drawing.Size(106, 20);
+            this.txbGroup.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(318, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Group (level)";
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(20, 129);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(215, 23);
+            this.btnCreate.TabIndex = 9;
+            this.btnCreate.Text = "Select File And Create CA";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(74, 219);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Solution for closing ticket";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(221, 216);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(295, 20);
+            this.txtResult.TabIndex = 11;
+            this.txtResult.Text = "Đã thực hiện.";
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(20, 168);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(215, 23);
+            this.btnClose.TabIndex = 12;
+            this.btnClose.Text = "Close Chrome";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // ckbAutoClose
+            // 
+            this.ckbAutoClose.AutoSize = true;
+            this.ckbAutoClose.Checked = true;
+            this.ckbAutoClose.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ckbAutoClose.Location = new System.Drawing.Point(321, 168);
+            this.ckbAutoClose.Name = "ckbAutoClose";
+            this.ckbAutoClose.Size = new System.Drawing.Size(74, 17);
+            this.ckbAutoClose.TabIndex = 13;
+            this.ckbAutoClose.Text = "AutoClose";
+            this.ckbAutoClose.UseVisualStyleBackColor = true;
+            // 
+            // txbAffected
+            // 
+            this.txbAffected.Location = new System.Drawing.Point(489, 101);
+            this.txbAffected.Name = "txbAffected";
+            this.txbAffected.Size = new System.Drawing.Size(27, 20);
+            this.txbAffected.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(318, 104);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(134, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Affected End User Position";
+            // 
+            // txbAssignee
+            // 
+            this.txbAssignee.Location = new System.Drawing.Point(489, 132);
+            this.txbAssignee.Name = "txbAssignee";
+            this.txbAssignee.Size = new System.Drawing.Size(27, 20);
+            this.txbAssignee.TabIndex = 17;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(318, 135);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(90, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Assignee Position";
+            // 
+            // AutoCA
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 265);
+            this.Controls.Add(this.txbAssignee);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txbAffected);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.ckbAutoClose);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.txbGroup);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txbName);
+            this.Controls.Add(this.label3);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AutoCA";
+            this.Text = "AutoCA";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txbUserName;
+        private System.Windows.Forms.TextBox txbPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txbGroup;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.CheckBox ckbAutoClose;
+        private System.Windows.Forms.TextBox txbAffected;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txbAssignee;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ AutoCA/AutoCA.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+using OpenQA.Selenium.Interactions;
+using System.Data.SqlClient;
+
+namespace AutoCA
+{    
+    public partial class AutoCA : Form
+    {
+        public static IWebDriver chromeDriver;
+        public static SqlConnection sqlConnection;
+        public AutoCA()
+        {
+            InitializeComponent();
+
+            //Init values from configuration file
+            txbUserName.Text = ConfigurationManager.AppSettings["UserName"];
+            txbPassword.Text = ConfigurationManager.AppSettings["Password"];
+            txbName.Text = ConfigurationManager.AppSettings["Name"];
+            txbGroup.Text = ConfigurationManager.AppSettings["Group"];
+            txbAffected.Text = ConfigurationManager.AppSettings["AffectedEndUserPosition"];
+            txbAssignee.Text = ConfigurationManager.AppSettings["AssigneePosition"];
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            if (!CheckInputData())
+            {
+                MessageBox.Show("Missing something");
+                return;
+            }
+
+            string fileName = "";
+            OpenFileDialog openFile = new OpenFileDialog();
+            DialogResult file = openFile.ShowDialog();
+            if (file == DialogResult.OK)
+            {
+                fileName = openFile.FileName;
+            }
+
+            try
+            {
+                DataTable data = ParseExcelFile(fileName);
+
+                data.Rows[0].Delete();
+                data.AcceptChanges();
+
+                //Create request
+                if (data.Rows.Count > 0)
+                {
+                    try
+                    {
+                        sqlConnection = new SqlConnection(ConfigurationManager.AppSettings["ConnectionString"]);
+                        sqlConnection.Open();
+                    }
+                    catch (Exception ex)
+                    {
+                        WriteLog(ex.ToString());
+                    }
+
+                    foreach (DataRow row in data.Rows)
+                    {
+                        if (string.IsNullOrEmpty(row[0].ToString()))
+                            continue;
+
+                        //Parsing Data
+                        string summary = row[1].ToString();
+                        string description = row[2].ToString();
+                        string requestArea = row[3].ToString();
+                        string rootCause = row[4].ToString();
+                        int catId = int.Parse(row[6].ToString());
+                        string caDate = row[7].ToString();
+
+                        //Create ticket on CA site
+                        ConnectAndSignIn();
+                        CreateRequest(summary, description, requestArea, rootCause, txbName.Text, txbGroup.Text);
+
+                        //Insert ticket to DB for reporting
+                        InsertDataToDB(sqlConnection, caDate, catId, summary, description, requestArea, rootCause);
+                    }
+                    sqlConnection.Close();
+                    sqlConnection.Dispose();
+                }
+                else
+                    WriteLog("Excel file doesnot contain data");
+
+                //Close Request
+                if (ckbAutoClose.Checked == true)
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            ConnectAndSignIn();
+                            CloseTicket(txtResult.Text);
+                        }
+                        catch
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                WriteLog(ex.ToString());
+            }
+        }
+
+        private void ConnectAndSignIn()
+        {
+            chromeDriver = new ChromeDriver();
+            chromeDriver.Manage().Window.Maximize();
+            chromeDriver.Url = "https://hotro.tct.vn/CAisd/SD";
+
+            chromeDriver.FindElement(By.Id("USERNAME")).SendKeys(txbUserName.Text);
+            chromeDriver.FindElement(By.Id("PIN")).SendKeys(txbPassword.Text);
+            chromeDriver.FindElement(By.Name("imgBtn0")).Click();
+        }
+
+        private void CreateRequest(string summary, string description, string requestName, 
+            string rootCauseName, string userName, string groupName)
+        {
+            Thread.Sleep(5000);
+
+            Actions selectFileMenu = new Actions(chromeDriver);
+            selectFileMenu.SendKeys(OpenQA.Selenium.Keys.LeftAlt + "f").Build().Perform();
+            Actions selectNewRequest = new Actions(chromeDriver);
+            selectNewRequest.SendKeys(OpenQA.Selenium.Keys.LeftAlt + "n").Build().Perform();
+
+            Thread.Sleep(2000);
+            //Go to frame which contains infor that we need to provide
+            IWebElement product = chromeDriver.FindElement(By.Name("product"));
+            chromeDriver.SwitchTo().Frame(product);
+            IWebElement tab_1003 = chromeDriver.FindElement(By.Id("tab_1003"));
+            chromeDriver.SwitchTo().Frame(tab_1003);
+            IWebElement role_main = chromeDriver.FindElement(By.Id("role_main"));
+            chromeDriver.SwitchTo().Frame(role_main);
+            IWebElement cai_main = chromeDriver.FindElement(By.Id("cai_main"));
+            chromeDriver.SwitchTo().Frame(cai_main);
+            
+            //Affected End User
+            chromeDriver.FindElement(By.Id("df_0_1")).SendKeys(userName);
+            Thread.Sleep(500);
+            string xPath = "/html/body/ul[2]/li[" + txbAffected.Text + "]/a";
+            chromeDriver.FindElement(By.XPath(xPath)).Click();
+            //Assignee User
+            chromeDriver.FindElement(By.Id("df_1_2")).SendKeys(userName);
+            Thread.Sleep(500);
+            xPath = "/html/body/ul[6]/li[" + txbAssignee.Text + "]/a";
+            chromeDriver.FindElement(By.XPath(xPath)).Click();
+
+            chromeDriver.FindElement(By.Id("df_1_1")).SendKeys(groupName);
+
+            chromeDriver.FindElement(By.Id("df_2_1")).SendKeys(rootCauseName);
+
+            chromeDriver.FindElement(By.Id("df_4_0")).SendKeys(summary);
+            chromeDriver.FindElement(By.Id("df_5_0")).SendKeys(description);
+
+            chromeDriver.FindElement(By.Id("df_0_2")).SendKeys(requestName);
+            Thread.Sleep(500);
+            chromeDriver.FindElement(By.XPath("/html/body/ul[3]/li/a")).Click();
+
+            Thread.Sleep(1000);
+            selectNewRequest = new Actions(chromeDriver);
+            selectNewRequest.SendKeys(OpenQA.Selenium.Keys.LeftAlt + "s").Build().Perform();
+            
+            Thread.Sleep(5000);
+            chromeDriver.Close();
+            chromeDriver.Dispose();
+        }
+
+        private void CloseTicket(string result)
+        {
+            Thread.Sleep(5000);//Wait for loading page
+
+            //Go to frame which contains infor that we need to provide
+            IWebElement product = chromeDriver.FindElement(By.Name("product"));
+            chromeDriver.SwitchTo().Frame(product);
+            IWebElement tab_1003 = chromeDriver.FindElement(By.Id("tab_1003"));
+            chromeDriver.SwitchTo().Frame(tab_1003);
+            IWebElement role_main = chromeDriver.FindElement(By.Id("role_main"));
+            chromeDriver.SwitchTo().Frame(role_main);
+            IWebElement scoreboard = chromeDriver.FindElement(By.Id("scoreboard"));
+            chromeDriver.SwitchTo().Frame(scoreboard);
+
+            chromeDriver.FindElement(By.XPath("//*[@id='s4pm']/span[2]")).Click();//My assign ticket
+            chromeDriver.FindElement(By.XPath("//*[@id='s14pm']/span[2]")).Click();//Request
+            chromeDriver.FindElement(By.XPath("//*[@id='s15ds']/span")).Click();//Open request
+
+            //Switch back to parent frame then go to frame containts all open ticket
+            chromeDriver.SwitchTo().ParentFrame();
+            IWebElement cai_main = chromeDriver.FindElement(By.Id("cai_main"));
+            chromeDriver.SwitchTo().Frame(cai_main);
+
+            Thread.Sleep(1000);
+            WriteLog("Closing Ticket: " + chromeDriver.FindElement(By.XPath("//*[@id='rslnk_0_0']")).Text);
+            chromeDriver.FindElement(By.XPath("//*[@id='rslnk_0_0']")).Click();
+
+            Thread.Sleep(2000);
+            Actions activityMenu = new Actions(chromeDriver);
+            activityMenu.SendKeys(OpenQA.Selenium.Keys.LeftAlt + "t").Build().Perform();
+            Actions updateStatusMenu = new Actions(chromeDriver);
+            updateStatusMenu.SendKeys(OpenQA.Selenium.Keys.LeftAlt + "u").Build().Perform();
+
+            Thread.Sleep(5000);
+            //Tick internal
+            chromeDriver.FindElement(By.Id("df_2_3")).Click();
+            //Choose closed
+            chromeDriver.FindElement(By.XPath("//*[@id='df_1_1']/option[1]")).Click();
+            chromeDriver.FindElement(By.Id("df_3_0")).SendKeys(result);
+            //Save new status
+            Actions saveNewStatus = new Actions(chromeDriver);
+            saveNewStatus.SendKeys(OpenQA.Selenium.Keys.LeftAlt + "s").Build().Perform();
+
+            Thread.Sleep(5000);
+            chromeDriver.Close();
+            chromeDriver.Dispose();
+        }
+
+        private bool CheckInputData()
+        {
+            if (string.IsNullOrWhiteSpace(txbGroup.Text))
+                return false;
+            if (string.IsNullOrWhiteSpace(txbName.Text))
+                return false;
+            if (string.IsNullOrWhiteSpace(txbPassword.Text))
+                return false;
+            if (string.IsNullOrWhiteSpace(txbUserName.Text))
+                return false;
+
+            return true;
+        }
+
+        private DataTable ParseExcelFile(string fileName)
+        {
+            DataTable results = new DataTable();
+            string sheetName = ConfigurationManager.AppSettings["SheetName"];
+
+            try
+            {
+                string connString = string.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 12.0;HDR=no'", fileName);
+                string sql = "SELECT * FROM [" + sheetName.ToString() + "]";
+                using (OleDbConnection conn = new OleDbConnection(connString))
+                {
+                    conn.Open();
+                    using (OleDbCommand cmd = new OleDbCommand(sql, conn))
+                    {
+                        using (OleDbDataReader rdr = cmd.ExecuteReader())
+                        {
+                            results.Load(rdr);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Couldnot parse file");
+            }
+            return results;
+        }
+
+        private static void WriteLog(string logData, bool logTimeStamp = true)
+        {
+            try
+            {
+                using (StreamWriter w = File.AppendText(ConfigurationManager.AppSettings.Get("LogFile")))
+                {
+                    if (logTimeStamp)
+                    {
+                        logData = DateTime.Now.ToLongDateString() + " " + DateTime.Now.ToLongTimeString() + ": " + logData;
+                    }
+                    w.WriteLine(logData);
+                }
+            }
+            catch
+            { }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            chromeDriver.Close();
+            chromeDriver.Dispose();
+        }
+
+        private void InsertDataToDB(SqlConnection sqlCon, string CADate, int CatId, string Summary, 
+            string Description, string RequestArea, string RootCause)
+        {
+            try
+            {
+                string command = "INSERT INTO DBO.CA (UserName, CADate, CatId, Summary, [Description], RequestArea, RootCause) "
+                + "VALUES (@userName,@caDate,@catId,@summary,@description,@requestArea,@rootCause)";
+                SqlCommand sqlCommand = new SqlCommand(command, sqlCon);
+                sqlCommand.Parameters.AddWithValue("@userName", txbUserName.Text);
+                sqlCommand.Parameters.AddWithValue("@caDate", CADate);
+                sqlCommand.Parameters.AddWithValue("@catId", CatId);
+                sqlCommand.Parameters.AddWithValue("@summary", Summary);
+                sqlCommand.Parameters.AddWithValue("@description", Description);
+                sqlCommand.Parameters.AddWithValue("@requestArea", RequestArea);
+                sqlCommand.Parameters.AddWithValue("@rootCause", RootCause);                
+
+                sqlCommand.ExecuteNonQuery();
+            }
+            catch(Exception ex)
+            {
+                WriteLog(ex.ToString());
+            }
+            
+        }        
+    }
+}
